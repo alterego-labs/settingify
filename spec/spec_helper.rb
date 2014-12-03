1d02d2c08f499fe27de26311b20bf22861927766
Sergey Gernyak <sergeg1990@gmail.com>
2014-12-03 11:30:18 +0200

    Setupping test environment

---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+$TESTING=true
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+
+require 'rspec/its'
+require 'settingify'
+
+Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
+
+RSpec.configure do |config|
+  config.mock_with :rspec
+
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+
+  config.order = "random"
+end
+
