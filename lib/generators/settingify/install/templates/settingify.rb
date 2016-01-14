69b0bf562d84e76bc9069435d55ca6483a02d2bf
Sergey Gernyak <sergeg1990@gmail.com>
2016-01-14 20:14:05 +0200

    Documenting a little

--- lib/generators/settingify/install/templates/settingify.rb
+++ lib/generators/settingify/install/templates/settingify.rb
@@ -1,3 +1,14 @@
+Settingify.config do |config|
+  # Uncomment if you want to use localization feature.
+  # For more information visit `lib/settingify/configs/localization.rb`
+  #
+  #config.localization do |localization|
+    #localization.active            = false
+    #localization.available_locales = []
+    #localization.default_locale    = :en
+  #end
+end
+
 Settingify.prepare_settings do
   # Your settings will be here
   # setting :my_setting, type: String, default: 'msg'
--- lib/settingify/configs/localization.rb
+++ lib/settingify/configs/localization.rb
@@ -1,8 +1,38 @@
 module Settingify
   module Configs
     class Localization
+      #
+      # Specifies if localization feature either is active or not.
+      #
+      # Values:
+      # true/false
+      #
+      # Default:
+      # false
+      #
       attr_accessor :active
+
+      #
+      # Contains locales array for that settingify must provides localized settings.
+      #
+      # Values:
+      # array of symbols
+      #
+      # Default:
+      # empty array
+      #
       attr_accessor :available_locales
+
+      #
+      # Specifies base locale.
+      # To this locale will be fall back if there is no value in the current locale
+      #
+      # Values:
+      # symbol
+      #
+      # Default:
+      # :en
+      #
       attr_accessor :default_locale
 
       def initialize
