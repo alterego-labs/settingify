9d2feb28dc3b195a744d5ad813735584c35ec772
Sergey Gernyak <sergeg1990@gmail.com>
2017-02-17 17:00:10 +0200

    Added repo for groups

---
+++ spec/lib/settingify/repos/groups_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe Settingify::Repos::Groups, :focus do
+  subject(:repo) { described_class.send :new }
+
+  describe '#all' do
+    it 'provides a list of defined groups' do
+      expect(repo.all).to be_a(Array)
+    end
+  end
+
+  describe '#add' do
+    it 'adds new item to list' do
+      expect { repo.add(:item) }.to change(repo.all, :count).by(1)
+    end
+  end
+
+  describe '#find_by_name' do
+    before do
+      repo.instance_variable_set(:@groups, groups)
+    end
+
+    let(:groups) do
+      [
+        Settingify::Data::Group.new('group1'),
+        Settingify::Data::Group.new('group2')
+      ]
+    end
+
+    context 'WHEN a group with a passed name exists' do
+      it 'returns it' do
+        group = repo.find_by_name('group1')
+        expect(group).to_not be_nil
+        expect(group.key).to eq 'group1'
+      end
+    end
+
+    context 'WHEN a group with a passed name does not exist' do
+      it 'returns nil' do
+        group = repo.find_by_name('group3')
+        expect(group).to be_nil
+      end
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,6 @@ Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 RSpec.configure do |config|
   config.mock_with :rspec
 
-  config.filter_run focus: true
   config.run_all_when_everything_filtered = true
 
   config.order = "random"
