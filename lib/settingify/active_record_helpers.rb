daf73369e9f92731e6bd89f385a4fbdfe2369c46
Sergio Gernyak <sergey.gernyak@diymaxeda.com>
2017-10-30 21:53:34 +0200

    :racehorse:momeize table existence checking
    To not make redundant SQL queries

--- lib/settingify/active_record_helpers.rb
+++ lib/settingify/active_record_helpers.rb
@@ -2,7 +2,7 @@ module Settingify
   class ActiveRecordHelpers
     class << self
       def table_exists?(table_name)
-        ActiveRecord::Base.connection.table_exists? table_name
+        @_table_exists ||= ActiveRecord::Base.connection.table_exists?(table_name)
       end
     end
   end
