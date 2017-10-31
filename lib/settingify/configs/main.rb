6e04afa28237b7b6dfb102cbf13ff898e097a757
Sergey Gernyak <sergeg1990@gmail.com>
2017-10-31 11:46:53 +0200

    :dizzy:extension for persistence repo
    - Changed a way to setup cache to hide some internal classes and be able
      to extend persistence repo silently
    - Also a new way is friendly for old users to not be pushy to setup
      cache

--- lib/settingify/configs/main.rb
+++ lib/settingify/configs/main.rb
@@ -1,11 +1,23 @@
 module Settingify
   module Configs
     class Main
-      attr_accessor :cache_store
+      attr_reader :cache_store
 
       def localization(&block)
         @_localization_config ||= Configs::Localization.new
         block_given? ? yield(@_localization_config) : @_localization_config
+      end
+
+      # Configure caching
+      #
+      # As a provider the Rails cache could be used. Basically, it is the main goal to use.
+      #
+      # As a side effect the method injects a caching extension inside persistence repo class.
+      #
+      # @param provider [#fetch, #delete_matched] - an instance of the cache provider
+      def setup_cache(provider:)
+        @cache_store = Settingify::Caching::Store.new(provider: provider)
+        Settingify::Persistence::Repo.send :prepend, Settingify::Caching::PersistenceRepoExtension
       end
     end
   end
